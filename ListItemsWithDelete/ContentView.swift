7c027ec8091430fcbe6cc9a880974325f667d18c
Joshua Hubbard <jhubardsf@gmail.com>
2020-01-10 23:04:15 -0500

    Add in example code and gif

--- ListItemsWithDelete/ContentView.swift
+++ ListItemsWithDelete/ContentView.swift
@@ -9,8 +9,26 @@
 import SwiftUI
 
 struct ContentView: View {
+    // 1.
+    @State private var animals = ["Dog", "Cat", "Cow", "Sheep", "Snake"]
+
     var body: some View {
-        Text("Hello, World!")
+    
+        NavigationView {
+            // 2.
+            List {
+                ForEach(animals, id: \.self) { animal in
+                    Text(animal)
+                }
+                // 3.
+                .onDelete(perform: self.deleteRow)
+            }
+            .navigationBarTitle(Text("Animals"))
+        }
+    }
+    // 4.
+    private func deleteRow(at indexSet: IndexSet) {
+        self.animals.remove(atOffsets: indexSet)
     }
 }
 
